2f48b33149b4b9f4d03730efeb9e42bdb8537a8a
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-10-02 15:24:31 +0200

    exos 9 & 10 done

--- exo_09.rb
+++ exo_09.rb
@@ -1,7 +1,8 @@
-print "Quelle est ton année de naissance ? "
-birthyear = gets.chomp.to_i
-today = 2023
-while birthyear <= today
-  puts birthyear
-  birthyear += 1
+puts "Quelle est ton année de naissance ?"
+print "> "
+birth_year = gets.chomp.to_i
+
+#opposite function to downto
+birth_year.upto(2023) do |i|
+  puts i
 end
--- exo_10.rb
+++ exo_10.rb
@@ -1,8 +1,15 @@
-print "Quelle est ton année de naissance ? "
-birthyear = gets.chomp.to_i
-today = 2023
-age = birthyear
-while birthyear <= today
-  puts "#{birthyear} #{birthyear - age} ans"
-  birthyear += 1
-end 
+puts "Quelle est ton année de naissance ?"
+print "> "
+birth_year = gets.chomp.to_i
+
+#opposite function to downto
+birth_year.upto(2023) do |i|
+  age = i - birth_year
+  if birth_year == i
+    puts "En #{i}, tu es né.e." #pour personnaliser le message l'année de naissance
+  elsif i == 2023
+    puts "En #{i}, tu as #{2023 - birth_year} ans." #pour mettre au présente uniquement sur l'année 2023
+  else #toutes les autres options
+    puts "En #{i}, tu avais #{i - birth_year} ans."
+  end
+end
