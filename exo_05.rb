4abeaf6185579647090e7afbca3a6aa0ceb506f4
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-10-02 14:34:01 +0200

    all exos from 1 to 6 done

--- exo_05.rb
+++ exo_05.rb
@@ -1,6 +1,11 @@
-puts "Hello, donne moi un nombre ?"
+puts "Donne-moi un nombre entre 1 et 9."
 print "> "
-num = gets.chomp
-num.to_i.times do
-puts "Salut, ça farte ?"
+
+number = gets.chomp.to_i #to get the number chosen bye the user
+if number > 0 && number < 10 #I add an if method so user can not choose a number other than the ones I want
+  number.times do
+    puts "Salut, ça farte ?"
+  end
+else
+  puts "Relis la consigne tête de noeud et recommence."
 end
--- exo_06.rb
+++ exo_06.rb
@@ -1,8 +1,10 @@
-puts "Hello, donne-moi un nombre."
+puts "Choisis un nombre entre 1 et 9 ?"
 print "> "
-num = gets.chomp.to_i
-index = 1
-while index <= (num -1)   
-  puts "Bonjour toi !"
-  index += 1
+number = gets.chomp.to_i
+if number > 0 && number < 10
+  (number - 1).times do
+    puts "Bonjour toi !"
+  end
+else
+  puts "Alors comment te dire qu'il fallait un chiffre entre 1 et 9, t'es bon pour recommencer Banane !"
 end
