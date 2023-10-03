1f24e72b185aacbf97a46b45d85122efde0a3348
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-10-03 10:51:06 +0200

    all exos done + some extra work to play a little bit further

--- exo_17.rb
+++ exo_17.rb
@@ -1,7 +1,30 @@
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (Entre 1 et 25)"
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
-stage = gets.chomp.to_i
-puts "Voici la pyramide :"
-(1..stage).each do |i|
-  puts (" " * (stage - i)) + ("#" * i) + ("#" * i)
+number = gets.chomp.to_i
+
+if number >= 1 && number <= 25
+  puts "Voici la pyramide :"
+  i = 1
+  j = number * 2 #comme en ligne 12 je ne prends que les pairs, ma pyramide aura deux fois moins d'étages, d'où le fois 2
+  while i <= j  do
+    spaces = (j - i) / 2
+    puts " " * spaces + "#" * i
+    i += 2 #puisque le seul moyen que ma pyramide soit centrée c'est que seuls les nombres pairs soient considérés.
+  end
+else
+  puts "Il me faut un nombre compris entre 1 et 25, recommence :"
+  print "> "
+  number_bis = gets.chomp.to_i
+  if number_bis >= 1 && number_bis <= 25
+    puts "Voici la pyramide :"
+    i_bis = 1
+    j_bis = number_bis * 2
+    while i_bis <= j_bis  do
+      spaces_bis = (j_bis - i_bis) / 2
+      puts " " * spaces_bis + "#" * i_bis
+      i_bis += 2 
+    end
+  else
+    puts "\u{1F635} Game Over \u{1F635}"
+  end
 end
