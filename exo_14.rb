21fd8dc8979206ff9432d810de87693dcbc866f8
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-10-03 15:46:29 +0200

    exo 14 updated bc was not exctly what it was asking for

--- exo_14.rb
+++ exo_14.rb
@@ -1,15 +1,19 @@
-n = 1 #initialise ma variable n à 1 -> cette variable me servira à générer les nums de mes emails fictifs
-m = 1 #variable d'indice qui sera utilisée dans la boucle ".each"
+emails_array = [] #créer mon array
 
-emails = [] #création de mon array
-while n <= 50 #boucle while qui s'exécute tant que n n'a pas atteint 50
-  emails << "rayon.soleil.#{n}@gmail.fr"
-  n +=1 #mon incrémentation
+1.upto(50) do |i|
+  if i < 10
+    email = "jean.dupont.0#{i}@email.fr" #pour ajouter le 0 devant les chiffres inférieurs à 10
+  else
+    email = "jean.dupont.#{i}@email.fr"
+  end
+  emails_array.push(email) #ajouter à chaque itération l'email généré au sein de l'array
 end
 
-emails.each do |email| #boucle each qui s'execute sur mon array emails, ici chaque élément de l'array est représenté par la variable email
-  if m % 2 == 0 #modulo de 2, ce qui signifie que si ce n'est pas divisible par 2 ce ne sera pas pris (donc si impaire = pas pris)
+j = 1 #variable d'indice qui sera utilisée dans la boucle ".each"
+emails_array.each do |email| #boucle each qui s'execute sur mon array emails, ici chaque élément de l'array est représenté par la variable email
+  if j % 2 == 0 #modulo de 2, ce qui signifie que si ce n'est pas divisible par 2 ce ne sera pas pris (donc si impaire = pas pris)
     p email #p imprime l'adresse email (inclut les infos de type objet)
+  else
   end
-  m +=1 #mon incrementation de 1 à chaque itération
+  j +=1 #mon incrementation de 1 à chaque itération
 end
